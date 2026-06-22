a202b82ba85aa6cb8e47680d9f1459db36c5a6da
Jeff Blume <jblume@childrenswi.org>
2026-06-22 13:50:44 -0500

    New final version of takingaction-4

---
+++ activities/takingaction-4/final2/audio/vo-captions.js
@@ -0,0 +1,772 @@
+export const Captions = {
+	"eng": {
+		"slide 10 tattling": [
+			{
+				"id": "1",
+				"startTime": 66,
+				"endTime": 1067,
+				"text": "You're right."
+			},
+			{
+				"id": "2",
+				"startTime": 1067,
+				"endTime": 3236,
+				"text": "While chewing gum\nmay be against the rules,"
+			},
+			{
+				"id": "3",
+				"startTime": 3236,
+				"endTime": 6306,
+				"text": "this is not a serious situation\nwhere someone could get hurt."
+			},
+			{
+				"id": "4",
+				"startTime": 6373,
+				"endTime": 7574,
+				"text": "Diego is tattling."
+			}
+		],
+		"slide 10 telling": [
+			{
+				"id": "1",
+				"startTime": 66,
+				"endTime": 3336,
+				"text": "This doesn't sound like a serious\nsituation where someone could get hurt."
+			},
+			{
+				"id": "2",
+				"startTime": 3403,
+				"endTime": 4571,
+				"text": "Diego is tattling."
+			}
+		],
+		"slide 10": [
+			{
+				"id": "1",
+				"startTime": 66,
+				"endTime": 1001,
+				"text": "Here's another one."
+			},
+			{
+				"id": "2",
+				"startTime": 1001,
+				"endTime": 4804,
+				"text": "Listen to this situation and decide\nif it's tattling or telling."
+			},
+			{
+				"id": "3",
+				"startTime": 4838,
+				"endTime": 6973,
+				"text": "After school,\nDiego says to his teacher,"
+			},
+			{
+				"id": "4",
+				"startTime": 6973,
+				"endTime": 9743,
+				"text": "“Mr. Stuart, Jamal was \nchewing gum in class today."
+			},
+			{
+				"id": "5",
+				"startTime": 9743,
+				"endTime": 11378,
+				"text": "He's always chewing gum.”"
+			},
+			{
+				"id": "6",
+				"startTime": 11378,
+				"endTime": 13213,
+				"text": "Is this tattling or telling?"
+			}
+		],
+		"slide 11 tattling": [
+			{
+				"id": "1",
+				"startTime": 100,
+				"endTime": 3670,
+				"text": "It sounds like this is a serious situation\nand Ben is being bullied."
+			},
+			{
+				"id": "2",
+				"startTime": 3703,
+				"endTime": 6306,
+				"text": "Telling an adult about \nbullying is not tattling."
+			}
+		],
+		"slide 11 telling": [
+			{
+				"id": "1",
+				"startTime": 66,
+				"endTime": 800,
+				"text": "Great job!"
+			},
+			{
+				"id": "2",
+				"startTime": 800,
+				"endTime": 3636,
+				"text": "It sounds like this\nbullying has been going on for a while."
+			},
+			{
+				"id": "3",
+				"startTime": 3636,
+				"endTime": 5739,
+				"text": "Ben did the right\nthing by telling an adult."
+			}
+		],
+		"slide 11": [
+			{
+				"id": "1",
+				"startTime": 66,
+				"endTime": 1334,
+				"text": "Let's try one more."
+			},
+			{
+				"id": "2",
+				"startTime": 1334,
+				"endTime": 5505,
+				"text": "Listen to the following situation\nand decide if it's tattling or telling."
+			},
+			{
+				"id": "3",
+				"startTime": 5972,
+				"endTime": 8475,
+				"text": "After class, Ben says to his teacher,"
+			},
+			{
+				"id": "4",
+				"startTime": 8475,
+				"endTime": 13713,
+				"text": "“Mr. Brewster, Zack has been messing with me\nevery day after school and he won't stop.”"
+			},
+			{
+				"id": "5",
+				"startTime": 13747,
+				"endTime": 15281,
+				"text": "Is this tattling or telling?"
+			}
+		],
+		"slide 2": [
+			{
+				"id": "1",
+				"startTime": 266,
+				"endTime": 2736,
+				"text": "There are a few ways you can take action."
+			},
+			{
+				"id": "2",
+				"startTime": 2736,
+				"endTime": 5772,
+				"text": "First of all,\nyou can speak up when you see bullying."
+			},
+			{
+				"id": "3",
+				"startTime": 5839,
+				"endTime": 9743,
+				"text": "You can simply tell the bully to stop\nor you can use a comeback."
+			},
+			{
+				"id": "4",
+				"startTime": 9843,
+				"endTime": 12612,
+				"text": "A comeback is just a quick or witty reply."
+			},
+			{
+				"id": "5",
+				"startTime": 12612,
+				"endTime": 16683,
+				"text": "If you are being bullied, you could say\nsomething as simple as “So what?”"
+			},
+			{
+				"id": "6",
+				"startTime": 16716,
+				"endTime": 19552,
+				"text": "or “Whatever” in a normal tone of voice."
+			},
+			{
+				"id": "7",
+				"startTime": 19552,
+				"endTime": 21021,
+				"text": "Bullies are usually looking"
+			},
+			{
+				"id": "8",
+				"startTime": 21021,
+				"endTime": 24157,
+				"text": "to get a reaction out of the person\nthey are bullying. By"
+			},
+			{
+				"id": "9",
+				"startTime": 24157,
+				"endTime": 27327,
+				"text": "using a simple comeback like this\nand responding calmly,"
+			},
+			{
+				"id": "10",
+				"startTime": 27360,
+				"endTime": 29295,
+				"text": "you don't give them the reaction they want"
+			},
+			{
+				"id": "11",
+				"startTime": 29295,
+				"endTime": 31798,
+				"text": "or that feeling of power\nthey're looking for."
+			},
+			{
+				"id": "12",
+				"startTime": 31798,
+				"endTime": 34801,
+				"text": "Another example of a comeback\nis thanking the bully"
+			},
+			{
+				"id": "13",
+				"startTime": 34801,
+				"endTime": 38171,
+				"text": "for their comments, or agreeing with them\nand saying that they're right."
+			},
+			{
+				"id": "14",
+				"startTime": 38238,
+				"endTime": 42142,
+				"text": "This type of response can be effective\nbecause you're not arguing with the bully"
+			},
+			{
+				"id": "15",
+				"startTime": 42142,
+				"endTime": 44477,
+				"text": "and giving them\nany of your time or energy."
+			},
+			{
+				"id": "16",
+				"startTime": 44477,
+				"endTime": 48148,
+				"text": "So again, the bully is not getting\nthe response that they want."
+			},
+			{
+				"id": "17",
+				"startTime": 48314,
+				"endTime": 51618,
+				"text": "Or you may want to try a strong,\nassertive comeback."
+			},
+			{
+				"id": "18",
+				"startTime": 51651,
+				"endTime": 53853,
+				"text": "That could be saying “Knock it off”,"
+			},
+			{
+				"id": "19",
+				"startTime": 53853,
+				"endTime": 57490,
+				"text": "“Leave me alone”,\nor “Stop it” using an assertive voice."
+			},
+			{
+				"id": "20",
+				"startTime": 57824,
+				"endTime": 60627,
+				"text": "You don't need to yell or be aggressive."
+			},
+			{
+				"id": "21",
+				"startTime": 60627,
+				"endTime": 63730,
+				"text": "This shows that you are confident\nthat you're not going to put up with"
+			},
+			{
+				"id": "22",
+				"startTime": 63730,
+				"endTime": 66966,
+				"text": "what they are doing, and you're not going\nto be mean back to them."
+			}
+		],
+		"slide 3": [
+			{
+				"id": "1",
+				"startTime": 66,
+				"endTime": 2602,
+				"text": "Let's\ntake a look at some different comebacks."
+			},
+			{
+				"id": "2",
+				"startTime": 2602,
+				"endTime": 4637,
+				"text": "Here's the first situation."
+			},
+			{
+				"id": "3",
+				"startTime": 4637,
+				"endTime": 6639,
+				"text": "Owen is really quiet at school."
+			},
+			{
+				"id": "4",
+				"startTime": 6639,
+				"endTime": 11211,
+				"text": "Lately a few kids in his class have been\ncalling him names and making fun of him."
+			},
+			{
+				"id": "5",
+				"startTime": 11378,
+				"endTime": 15315,
+				"text": "They'll even say to his face\nthat he needs to shower because he smells."
+			},
+			{
+				"id": "6",
+				"startTime": 15782,
+				"endTime": 18018,
+				"text": "This has been going on for a while now."
+			},
+			{
+				"id": "7",
+				"startTime": 18018,
+				"endTime": 23323,
+				"text": "Today, Caden walks past his desk, sniffs\nthe air and says, what stinks over here?"
+			},
+			{
+				"id": "8",
+				"startTime": 23857,
+				"endTime": 26826,
+				"text": "Owen decides that he needs\nto do something about it."
+			},
+			{
+				"id": "9",
+				"startTime": 26826,
+				"endTime": 31264,
+				"text": "Click on each comeback button to watch\nOwen use different types of comebacks."
+			}
+		],
+		"slide 4": [
+			{
+				"id": "1",
+				"startTime": 66,
+				"endTime": 3103,
+				"text": "Let's take a look at another bullying\nsituation."
+			},
+			{
+				"id": "2",
+				"startTime": 3269,
+				"endTime": 7040,
+				"text": "Sofia is at her locker when she hears\ngiggling behind her."
+			},
+			{
+				"id": "3",
+				"startTime": 7073,
+				"endTime": 11378,
+				"text": "Then Ava accidentally bumps into her,\ncausing all of her things to fall."
+			},
+			{
+				"id": "4",
+				"startTime": 11811,
+				"endTime": 14814,
+				"text": "This is not the first time\nsomething like this has happened."
+			},
+			{
+				"id": "5",
+				"startTime": 15015,
+				"endTime": 19419,
+				"text": "Click on each comeback button to watch\nSofia use different types of comebacks."
+			}
+		],
+		"slide 5": [
+			{
+				"id": "1",
+				"startTime": 100,
+				"endTime": 2936,
+				"text": "Another way to take action is by seeking\nhelp."
+			},
+			{
+				"id": "2",
+				"startTime": 2936,
+				"endTime": 5638,
+				"text": "There are many different people\nyou can seek help from."
+			},
+			{
+				"id": "3",
+				"startTime": 5638,
+				"endTime": 7941,
+				"text": "Think of all the trusted adults you know:"
+			},
+			{
+				"id": "4",
+				"startTime": 7941,
+				"endTime": 9743,
+				"text": "a parent or a caregiver,"
+			},
+			{
+				"id": "5",
+				"startTime": 9743,
+				"endTime": 11344,
+				"text": "grandparent, teacher,"
+			},
+			{
+				"id": "6",
+				"startTime": 11344,
+				"endTime": 12412,
+				"text": "the school nurse,"
+			},
+			{
+				"id": "7",
+				"startTime": 12412,
+				"endTime": 15682,
+				"text": "counselor,\nprincipal, school social worker,"
+			},
+			{
+				"id": "8",
+				"startTime": 15682,
+				"endTime": 19185,
+				"text": "school psychologist or any other adult\nyou trust."
+			},
+			{
+				"id": "9",
+				"startTime": 19219,
+				"endTime": 21988,
+				"text": "These adults can help\nmake sure the bullying stops."
+			},
+			{
+				"id": "10",
+				"startTime": 21988,
+				"endTime": 24557,
+				"text": "And if the first adult you go to doesn't\nlisten,"
+			},
+			{
+				"id": "11",
+				"startTime": 24557,
+				"endTime": 26359,
+				"text": "find another one that will."
+			},
+			{
+				"id": "12",
+				"startTime": 26359,
+				"endTime": 29195,
+				"text": "Even if you are able\nto get the bullying to stop,"
+			},
+			{
+				"id": "13",
+				"startTime": 29195,
+				"endTime": 31931,
+				"text": "an adult\nshould be aware of what's going on."
+			},
+			{
+				"id": "14",
+				"startTime": 31931,
+				"endTime": 35001,
+				"text": "Your friends could also be helpful\nin dealing with bullying."
+			},
+			{
+				"id": "15",
+				"startTime": 35035,
+				"endTime": 37837,
+				"text": "They can help you figure\nout what to do and say,"
+			},
+			{
+				"id": "16",
+				"startTime": 37837,
+				"endTime": 40106,
+				"text": "and they can be a great \nsource of support."
+			},
+			{
+				"id": "17",
+				"startTime": 40140,
+				"endTime": 43076,
+				"text": "Friends can be there for you\nif you need or want to talk."
+			}
+		],
+		"slide 6 empathy": [
+			{
+				"id": "1",
+				"startTime": 66,
+				"endTime": 1601,
+				"text": "If you're uncomfortable stepping"
+			},
+			{
+				"id": "2",
+				"startTime": 1601,
+				"endTime": 5338,
+				"text": "in during the situation, talk\nto the person who's being bullied later."
+			},
+			{
+				"id": "3",
+				"startTime": 5372,
+				"endTime": 8742,
+				"text": "Tell them you're sorry about\nwhat happened to them and it's not okay."
+			},
+			{
+				"id": "4",
+				"startTime": 8775,
+				"endTime": 12112,
+				"text": "This is called showing empathy.\nShowing that you care"
+			},
+			{
+				"id": "5",
+				"startTime": 12112,
+				"endTime": 15315,
+				"text": "and that you understand\nhow they feel makes them feel less alone."
+			}
+		],
+		"slide 6 go with": [
+			{
+				"id": "1",
+				"startTime": 100,
+				"endTime": 3503,
+				"text": "Offer to go with the person being bullied\nto report the bullying."
+			},
+			{
+				"id": "2",
+				"startTime": 3536,
+				"endTime": 5372,
+				"text": "They may be afraid to tell an adult,"
+			},
+			{
+				"id": "3",
+				"startTime": 5372,
+				"endTime": 8775,
+				"text": "or they may think telling an adult\nisn't really going to help."
+			},
+			{
+				"id": "4",
+				"startTime": 8875,
+				"endTime": 12445,
+				"text": "Showing your support will make them\nmore likely to get the help they need."
+			}
+		],
+		"slide 6 help": [
+			{
+				"id": "1",
+				"startTime": 66,
+				"endTime": 3136,
+				"text": "Help the person\nbeing bullied get out of the situation."
+			},
+			{
+				"id": "2",
+				"startTime": 3203,
+				"endTime": 4537,
+				"text": "That could be as simple"
+			},
+			{
+				"id": "3",
+				"startTime": 4537,
+				"endTime": 7674,
+				"text": "as putting your arm around them, saying\n“Hey, come on. Let's go.”"
+			}
+		],
+		"slide 6 invite": [
+			{
+				"id": "1",
+				"startTime": 66,
+				"endTime": 1701,
+				"text": "Invite the person who’s\nbeing bullied"
+			},
+			{
+				"id": "2",
+				"startTime": 1701,
+				"endTime": 4571,
+				"text": "to join you and your friends \nat recess or lunch."
+			},
+			{
+				"id": "3",
+				"startTime": 4637,
+				"endTime": 7741,
+				"text": "This makes them feel included\nand part of the school community."
+			}
+		],
+		"slide 6 outro": [
+			{
+				"id": "1",
+				"startTime": 133,
+				"endTime": 1401,
+				"text": "The important thing is that"
+			},
+			{
+				"id": "2",
+				"startTime": 1401,
+				"endTime": 4371,
+				"text": "you reach out in some way\nto help the person being bullied."
+			},
+			{
+				"id": "3",
+				"startTime": 4537,
+				"endTime": 7273,
+				"text": "You don't have to be their best friend\nto help them."
+			},
+			{
+				"id": "4",
+				"startTime": 7273,
+				"endTime": 10577,
+				"text": "It’s every student's responsibility\nto stand up to bullying and to"
+			},
+			{
+				"id": "5",
+				"startTime": 10610,
+				"endTime": 12078,
+				"text": "show respect for others."
+			}
+		],
+		"slide 6": [
+			{
+				"id": "1",
+				"startTime": 66,
+				"endTime": 3737,
+				"text": "You may find yourself in a situation\nwhere you're not the one being bullied,"
+			},
+			{
+				"id": "2",
+				"startTime": 3737,
+				"endTime": 6039,
+				"text": "but you see someone else being bullied."
+			},
+			{
+				"id": "3",
+				"startTime": 6039,
+				"endTime": 8174,
+				"text": "That means you are a bystander."
+			},
+			{
+				"id": "4",
+				"startTime": 8174,
+				"endTime": 9909,
+				"text": "Click the buttons to hear about things"
+			},
+			{
+				"id": "5",
+				"startTime": 9909,
+				"endTime": 13947,
+				"text": "that you can do to become an upstander\nand help the person being bullied."
+			}
+		],
+		"slide 7": [
+			{
+				"id": "1",
+				"startTime": 66,
+				"endTime": 3470,
+				"text": "Let's go back to our example of Owen\nbeing bullied in the classroom"
+			},
+			{
+				"id": "2",
+				"startTime": 3470,
+				"endTime": 7073,
+				"text": "and see what would’ve happened if\nsomeone would have stepped in to help him."
+			},
+			{
+				"id": "3",
+				"startTime": 7073,
+				"endTime": 8441,
+				"text": "Click on each bystander"
+			},
+			{
+				"id": "4",
+				"startTime": 8441,
+				"endTime": 11678,
+				"text": "button to see a different way\nthat a bystander could help Owen."
+			}
+		],
+		"slide 8": [
+			{
+				"id": "1",
+				"startTime": 100,
+				"endTime": 3470,
+				"text": "Sometimes the bystanders or victim\ndon't tell an adult"
+			},
+			{
+				"id": "2",
+				"startTime": 3470,
+				"endTime": 5872,
+				"text": "because they think it's tattling\nor snitching."
+			},
+			{
+				"id": "3",
+				"startTime": 5872,
+				"endTime": 8875,
+				"text": "Well, there's a big difference\nbetween tattling and telling."
+			},
+			{
+				"id": "4",
+				"startTime": 8875,
+				"endTime": 12545,
+				"text": "Tattling is usually done to try\nand get someone in trouble over something"
+			},
+			{
+				"id": "5",
+				"startTime": 12545,
+				"endTime": 14280,
+				"text": "that's not very important."
+			},
+			{
+				"id": "6",
+				"startTime": 14280,
+				"endTime": 18118,
+				"text": "Telling is when there's a serious\nsituation and someone could get hurt."
+			},
+			{
+				"id": "7",
+				"startTime": 18151,
+				"endTime": 21788,
+				"text": "All bullying situations are serious\nand an adult needs to be told."
+			}
+		],
+		"slide 9 tattling": [
+			{
+				"id": "1",
+				"startTime": 100,
+				"endTime": 3203,
+				"text": "This sounds like a serious situation\nthat an adult should know about,"
+			},
+			{
+				"id": "2",
+				"startTime": 3203,
+				"endTime": 5638,
+				"text": "which means it's\ntelling. Dezi could be hurt."
+			}
+		],
+		"slide 9 telling": [
+			{
+				"id": "1",
+				"startTime": 100,
+				"endTime": 1001,
+				"text": "Great job!"
+			},
+			{
+				"id": "2",
+				"startTime": 1001,
+				"endTime": 4037,
+				"text": "This sounds like a serious situation\nthat an adult should know about,"
+			},
+			{
+				"id": "3",
+				"startTime": 4037,
+				"endTime": 6439,
+				"text": "which means it's\ntelling. Dezi could be hurt."
+			}
+		],
+		"slide 9": [
+			{
+				"id": "1",
+				"startTime": 100,
+				"endTime": 3770,
+				"text": "Let's take a closer look at the difference\nbetween tattling and telling."
+			},
+			{
+				"id": "2",
+				"startTime": 3837,
+				"endTime": 8041,
+				"text": "Listen to the following situation\nand decide if it's tattling or telling."
+			},
+			{
+				"id": "3",
+				"startTime": 8308,
+				"endTime": 10977,
+				"text": "After recess Jayden finds \nher teacher and says,"
+			},
+			{
+				"id": "4",
+				"startTime": 10977,
+				"endTime": 15582,
+				"text": "“I saw Malik push Dezi on the playground\nand Dezi looks really upset.”"
+			},
+			{
+				"id": "5",
+				"startTime": 15682,
+				"endTime": 17450,
+				"text": "Is this tattling or telling?"
+			}
+		]
+	}
+}
