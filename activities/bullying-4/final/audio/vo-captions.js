8172adfd4ea1e31234ecec9385078e921d56d771
Jeff Blume <jblume@childrenswi.org>
2026-06-23 13:36:24 -0500

    Add bullying-4 final

---
+++ activities/bullying-4/final/audio/vo-captions.js
@@ -0,0 +1,634 @@
+export const Captions = {
+	"eng": {
+		"slide 2 1": [
+			{
+				"id": "1",
+				"startTime": 66,
+				"endTime": 3036,
+				"text": "It is intentional,\nwhich means it's done on purpose."
+			},
+			{
+				"id": "2",
+				"startTime": 3036,
+				"endTime": 4804,
+				"text": "It isn't an accident."
+			}
+		],
+		"slide 2 2": [
+			{
+				"id": "1",
+				"startTime": 100,
+				"endTime": 1735,
+				"text": "It is repeated over time,"
+			},
+			{
+				"id": "2",
+				"startTime": 1735,
+				"endTime": 3570,
+				"text": "or it happens more than once."
+			}
+		],
+		"slide 2 3": [
+			{
+				"id": "1",
+				"startTime": 100,
+				"endTime": 2002,
+				"text": "There is an imbalance of power,"
+			},
+			{
+				"id": "2",
+				"startTime": 2002,
+				"endTime": 5872,
+				"text": "which means the bully has more power\nthan the victim in some way."
+			},
+			{
+				"id": "3",
+				"startTime": 5905,
+				"endTime": 10210,
+				"text": "For example, they may be older,\nbigger in size or more popular."
+			}
+		],
+		"slide 2": [
+			{
+				"id": "1",
+				"startTime": 133,
+				"endTime": 2402,
+				"text": "First, we need to define bullying."
+			},
+			{
+				"id": "2",
+				"startTime": 2402,
+				"endTime": 4070,
+				"text": "Bullying is mean or hurtful"
+			},
+			{
+				"id": "3",
+				"startTime": 4070,
+				"endTime": 8074,
+				"text": "behavior done on purpose\nto make someone feel bad or unsafe."
+			},
+			{
+				"id": "4",
+				"startTime": 8241,
+				"endTime": 11411,
+				"text": "Not all unkind\nbehaviors are bullying, though."
+			},
+			{
+				"id": "5",
+				"startTime": 11511,
+				"endTime": 14914,
+				"text": "Three things are usually true for it\nto be considered bullying."
+			},
+			{
+				"id": "6",
+				"startTime": 14981,
+				"endTime": 16916,
+				"text": "Click the numbers to learn what they are."
+			}
+		],
+		"slide 3 cyberbullying": [
+			{
+				"id": "1",
+				"startTime": 133,
+				"endTime": 2936,
+				"text": "Cyberbullying\nis bullying that takes place over"
+			},
+			{
+				"id": "2",
+				"startTime": 2936,
+				"endTime": 6740,
+				"text": "digital devices\nlike cell phones, computers and tablets."
+			},
+			{
+				"id": "3",
+				"startTime": 6773,
+				"endTime": 11745,
+				"text": "It's using technology to repeatedly\nharass, threaten or embarrass someone."
+			},
+			{
+				"id": "4",
+				"startTime": 11778,
+				"endTime": 15515,
+				"text": "It could include: texting, emailing\nor sending mean,"
+			},
+			{
+				"id": "5",
+				"startTime": 15548,
+				"endTime": 19953,
+				"text": "threatening or rude messages\nto someone, posting mean, embarrassing"
+			},
+			{
+				"id": "6",
+				"startTime": 19953,
+				"endTime": 23323,
+				"text": "or hurtful things about someone online\nand commenting"
+			},
+			{
+				"id": "7",
+				"startTime": 23323,
+				"endTime": 26459,
+				"text": "on someone's social media posts\nin a hurtful, negative way."
+			}
+		],
+		"slide 3 outro": [
+			{
+				"id": "1",
+				"startTime": 100,
+				"endTime": 4270,
+				"text": "It's important to always stop and think\nbefore you post something or text someone."
+			},
+			{
+				"id": "2",
+				"startTime": 4304,
+				"endTime": 7140,
+				"text": "You shouldn't say anything online\nthat you wouldn't be willing"
+			},
+			{
+				"id": "3",
+				"startTime": 7140,
+				"endTime": 8541,
+				"text": "to say to someone's face."
+			},
+			{
+				"id": "4",
+				"startTime": 8541,
+				"endTime": 12579,
+				"text": "And remember, anything you post or send\ncould be shared to others"
+			},
+			{
+				"id": "5",
+				"startTime": 12579,
+				"endTime": 14014,
+				"text": "and saved by someone."
+			},
+			{
+				"id": "6",
+				"startTime": 14014,
+				"endTime": 17317,
+				"text": "Anything you post online lasts forever."
+			}
+		],
+		"slide 3 physical": [
+			{
+				"id": "1",
+				"startTime": 66,
+				"endTime": 3903,
+				"text": "Physical bullying involves\nhurting a person's body or their things"
+			},
+			{
+				"id": "2",
+				"startTime": 3903,
+				"endTime": 7307,
+				"text": "and could include hitting, kicking,\nspitting,"
+			},
+			{
+				"id": "3",
+				"startTime": 7340,
+				"endTime": 11244,
+				"text": "tripping, pushing\nand taking or breaking someone's things."
+			}
+		],
+		"slide 3 social": [
+			{
+				"id": "1",
+				"startTime": 133,
+				"endTime": 4270,
+				"text": "Social bullying involves hurting someone's\nrelationship or reputation,"
+			},
+			{
+				"id": "2",
+				"startTime": 4270,
+				"endTime": 7874,
+				"text": "and could include\nleaving a person out on purpose,"
+			},
+			{
+				"id": "3",
+				"startTime": 7907,
+				"endTime": 9442,
+				"text": "telling others not to be friends"
+			},
+			{
+				"id": "4",
+				"startTime": 9442,
+				"endTime": 13813,
+				"text": "with someone, spreading rumors\nand embarrassing someone in public."
+			}
+		],
+		"slide 3 verbal": [
+			{
+				"id": "1",
+				"startTime": 200,
+				"endTime": 4571,
+				"text": "Verbal bullying is when someone\nuses their words to hurt or scare someone."
+			},
+			{
+				"id": "2",
+				"startTime": 4637,
+				"endTime": 7574,
+				"text": "It could include teasing, name calling,"
+			},
+			{
+				"id": "3",
+				"startTime": 7574,
+				"endTime": 10577,
+				"text": "making inappropriate comments\nabout someone's body,"
+			},
+			{
+				"id": "4",
+				"startTime": 10610,
+				"endTime": 13013,
+				"text": "taunting and threatening to harm someone."
+			}
+		],
+		"slide 3": [
+			{
+				"id": "1",
+				"startTime": 100,
+				"endTime": 2202,
+				"text": "There\nare several different types of bullying."
+			},
+			{
+				"id": "2",
+				"startTime": 2202,
+				"endTime": 6673,
+				"text": "We're going to learn about physical,\nverbal, social and cyberbullying."
+			},
+			{
+				"id": "3",
+				"startTime": 6706,
+				"endTime": 8641,
+				"text": "Click on each type to learn more."
+			}
+		],
+		"slide 4": [
+			{
+				"id": "1",
+				"startTime": 100,
+				"endTime": 4304,
+				"text": "Sometimes it can be hard to tell\nif a behavior is bullying or not."
+			},
+			{
+				"id": "2",
+				"startTime": 4337,
+				"endTime": 8108,
+				"text": "We know that everyone doesn't\nget along all the time, but that doesn't"
+			},
+			{
+				"id": "3",
+				"startTime": 8108,
+				"endTime": 12479,
+				"text": "mean that every time someone disagrees\nabout something, bullying is occurring."
+			},
+			{
+				"id": "4",
+				"startTime": 12512,
+				"endTime": 15915,
+				"text": "Same thing with name\ncalling and other unkind behaviors."
+			},
+			{
+				"id": "5",
+				"startTime": 15949,
+				"endTime": 19352,
+				"text": "Conflict is a normal part of life\nand it can happen when there are"
+			},
+			{
+				"id": "6",
+				"startTime": 19352,
+				"endTime": 24024,
+				"text": "differences in opinions,\nneeds, wants, values or ideas."
+			},
+			{
+				"id": "7",
+				"startTime": 24057,
+				"endTime": 28061,
+				"text": "Examples of conflict\ninclude arguing over a game, disagreeing"
+			},
+			{
+				"id": "8",
+				"startTime": 28061,
+				"endTime": 32732,
+				"text": "about what movie to watch or arguing over\nwho does what for a school project."
+			},
+			{
+				"id": "9",
+				"startTime": 32799,
+				"endTime": 36503,
+				"text": "Conflicts can be solved\nby talking, listening and finding a way"
+			},
+			{
+				"id": "10",
+				"startTime": 36503,
+				"endTime": 38238,
+				"text": "to fix the problem together."
+			}
+		],
+		"slide 5 feelings": [
+			{
+				"id": "1",
+				"startTime": 100,
+				"endTime": 1835,
+				"text": "How does each person feel?"
+			},
+			{
+				"id": "2",
+				"startTime": 1835,
+				"endTime": 4571,
+				"text": "If both people are feeling frustrated\nor mad,"
+			},
+			{
+				"id": "3",
+				"startTime": 4571,
+				"endTime": 6072,
+				"text": "it's probably a conflict."
+			},
+			{
+				"id": "4",
+				"startTime": 6072,
+				"endTime": 10610,
+				"text": "But if only one person is feeling sad\nor hurt, then it's most likely bullying."
+			}
+		],
+		"slide 5 power": [
+			{
+				"id": "1",
+				"startTime": 66,
+				"endTime": 2602,
+				"text": "Does one\nperson have more power than the other?"
+			},
+			{
+				"id": "2",
+				"startTime": 2602,
+				"endTime": 6506,
+				"text": "Maybe they're bigger, older\nor more popular than the other person."
+			},
+			{
+				"id": "3",
+				"startTime": 6539,
+				"endTime": 9909,
+				"text": "If so, it's probably a bullying situation."
+			}
+		],
+		"slide 5 repeated": [
+			{
+				"id": "1",
+				"startTime": 66,
+				"endTime": 1935,
+				"text": "Has it happened more than once?"
+			},
+			{
+				"id": "2",
+				"startTime": 1935,
+				"endTime": 5672,
+				"text": "If similar situations\nkeep happening, it's most likely bullying."
+			}
+		],
+		"slide 5 sides": [
+			{
+				"id": "1",
+				"startTime": 100,
+				"endTime": 2302,
+				"text": "Is it one sided or two sided?"
+			},
+			{
+				"id": "2",
+				"startTime": 2302,
+				"endTime": 6239,
+				"text": "For example, if two people are always\ninsulting each other or talking bad"
+			},
+			{
+				"id": "3",
+				"startTime": 6239,
+				"endTime": 10143,
+				"text": "about the other person behind their back,\nthat means it's two sided."
+			},
+			{
+				"id": "4",
+				"startTime": 10343,
+				"endTime": 11745,
+				"text": "They're both being unkind."
+			},
+			{
+				"id": "5",
+				"startTime": 11745,
+				"endTime": 14414,
+				"text": "So that's conflict between two people."
+			},
+			{
+				"id": "6",
+				"startTime": 14414,
+				"endTime": 17817,
+				"text": "But if one person is always talking\nabout someone behind their back"
+			},
+			{
+				"id": "7",
+				"startTime": 17817,
+				"endTime": 20420,
+				"text": "or insulting them to their face\nand the other person"
+			},
+			{
+				"id": "8",
+				"startTime": 20420,
+				"endTime": 24357,
+				"text": "isn't doing the same,\nthen it's one sided and that's bullying."
+			}
+		],
+		"slide 5 solution": [
+			{
+				"id": "1",
+				"startTime": 66,
+				"endTime": 2936,
+				"text": "Can it be solved by talking and listening\nto each other?"
+			},
+			{
+				"id": "2",
+				"startTime": 2936,
+				"endTime": 6139,
+				"text": "If yes, it's most likely conflict.\nWith bullying,"
+			},
+			{
+				"id": "3",
+				"startTime": 6172,
+				"endTime": 8341,
+				"text": "an adult is usually needed to help."
+			}
+		],
+		"slide 5": [
+			{
+				"id": "1",
+				"startTime": 66,
+				"endTime": 1267,
+				"text": "Here are some questions you"
+			},
+			{
+				"id": "2",
+				"startTime": 1267,
+				"endTime": 5505,
+				"text": "can ask yourself to determine\nif a situation is bullying or conflict."
+			},
+			{
+				"id": "3",
+				"startTime": 5538,
+				"endTime": 7273,
+				"text": "Click on each one to learn more."
+			}
+		],
+		"slide 6": [
+			{
+				"id": "1",
+				"startTime": 66,
+				"endTime": 1434,
+				"text": "Bullying affects everyone,"
+			},
+			{
+				"id": "2",
+				"startTime": 1434,
+				"endTime": 5438,
+				"text": "not just the person who is bullying others\nand the person being bullied."
+			},
+			{
+				"id": "3",
+				"startTime": 5505,
+				"endTime": 9509,
+				"text": "The person who is being bullied can feel\nsad, hurt, lonely,"
+			},
+			{
+				"id": "4",
+				"startTime": 9509,
+				"endTime": 14314,
+				"text": "scared, confused and even angry,\nand the person bullying others"
+			},
+			{
+				"id": "5",
+				"startTime": 14314,
+				"endTime": 18518,
+				"text": "often feels angry, unhappy\nand sometimes even guilty."
+			},
+			{
+				"id": "6",
+				"startTime": 18551,
+				"endTime": 21755,
+				"text": "They may be dealing with things\nin their own life and don't know how to"
+			},
+			{
+				"id": "7",
+				"startTime": 21788,
+				"endTime": 22922,
+				"text": "handle their feelings."
+			}
+		],
+		"slide 7 afraid": [
+			{
+				"id": "1",
+				"startTime": 100,
+				"endTime": 934,
+				"text": "Afraid."
+			},
+			{
+				"id": "2",
+				"startTime": 934,
+				"endTime": 3470,
+				"text": "Seeing someone being bullied can be scary."
+			},
+			{
+				"id": "3",
+				"startTime": 3470,
+				"endTime": 8308,
+				"text": "Bystanders may feel afraid that if they\nstep in to help, they may be bullied too."
+			}
+		],
+		"slide 7 angry": [
+			{
+				"id": "1",
+				"startTime": 133,
+				"endTime": 2936,
+				"text": "Angry. Bystanders may feel angry."
+			},
+			{
+				"id": "2",
+				"startTime": 2936,
+				"endTime": 7207,
+				"text": "They may be angry that someone is\nbullying others and no one is stopping it."
+			}
+		],
+		"slide 7 confused": [
+			{
+				"id": "1",
+				"startTime": 66,
+				"endTime": 4371,
+				"text": "Confused. Bystanders may feel confused\nbecause they may not understand"
+			},
+			{
+				"id": "2",
+				"startTime": 4371,
+				"endTime": 5438,
+				"text": "what is happening."
+			},
+			{
+				"id": "3",
+				"startTime": 5438,
+				"endTime": 8441,
+				"text": "They might wonder if they should\ntell an adult or stay quiet."
+			}
+		],
+		"slide 7 guilty": [
+			{
+				"id": "1",
+				"startTime": 100,
+				"endTime": 3436,
+				"text": "Guilty. Depending on what they did\nor didn't do to help,"
+			},
+			{
+				"id": "2",
+				"startTime": 3470,
+				"endTime": 5472,
+				"text": "bystanders may feel guilty."
+			},
+			{
+				"id": "3",
+				"startTime": 5472,
+				"endTime": 9142,
+				"text": "They may wonder if they did the right\nthing or if they could have done more."
+			}
+		],
+		"slide 7 sad": [
+			{
+				"id": "1",
+				"startTime": 133,
+				"endTime": 4704,
+				"text": "Sad. Seeing someone get hurt or left out\ncan make bystanders feel sad."
+			},
+			{
+				"id": "2",
+				"startTime": 4738,
+				"endTime": 7173,
+				"text": "They may feel like\nthere is nothing they can do to help."
+			}
+		],
+		"slide 7": [
+			{
+				"id": "1",
+				"startTime": 100,
+				"endTime": 3203,
+				"text": "Those\nwho witness bullying are affected, too."
+			},
+			{
+				"id": "2",
+				"startTime": 3236,
+				"endTime": 6573,
+				"text": "They're often called bystanders,\nand they can have many feelings"
+			},
+			{
+				"id": "3",
+				"startTime": 6573,
+				"endTime": 8708,
+				"text": "from seeing others being bullied."
+			},
+			{
+				"id": "4",
+				"startTime": 8708,
+				"endTime": 10677,
+				"text": "Click on each feeling to learn more."
+			}
+		]
+	}
+}
