c0e3c4193b1b09559fbea1b6134287d5b36e8474
Jeff Blume <jblume@childrenswi.org>
2026-06-29 13:18:40 -0500

    Add zinkthezebra-4

---
+++ activities/zinkthezebra-4/audio/vo-captions.js
@@ -0,0 +1,160 @@
+export const Captions = {
+	"eng": {
+		"Intro 1": [
+			{
+				"id": "1",
+				"startTime": 66,
+				"endTime": 1835,
+				"text": "Welcome to Zink’s story."
+			},
+			{
+				"id": "2",
+				"startTime": 1835,
+				"endTime": 6139,
+				"text": "Zink stands for understanding,\nrespect, compassion, and acceptance."
+			},
+			{
+				"id": "3",
+				"startTime": 6172,
+				"endTime": 8875,
+				"text": "Her story shares\nthe importance of kindness."
+			},
+			{
+				"id": "4",
+				"startTime": 8875,
+				"endTime": 11878,
+				"text": "Everyone is different,\nand that's not a bad thing."
+			},
+			{
+				"id": "5",
+				"startTime": 11878,
+				"endTime": 15482,
+				"text": "Regardless of our interests,\nabilities, cultural background"
+			},
+			{
+				"id": "6",
+				"startTime": 15482,
+				"endTime": 19919,
+				"text": "or physical differences, everyone wants\nto feel accepted and valued."
+			},
+			{
+				"id": "7",
+				"startTime": 19986,
+				"endTime": 22355,
+				"text": "Click next to hear Zink’s story."
+			}
+		],
+		"Outro 1": [
+			{
+				"id": "1",
+				"startTime": 133,
+				"endTime": 1634,
+				"text": "Zink was treated differently"
+			},
+			{
+				"id": "2",
+				"startTime": 1634,
+				"endTime": 5505,
+				"text": "because she has spots instead of stripes,\nso she looks different."
+			},
+			{
+				"id": "3",
+				"startTime": 5505,
+				"endTime": 7974,
+				"text": "But we should be\ncelebrating our differences."
+			},
+			{
+				"id": "4",
+				"startTime": 7974,
+				"endTime": 10877,
+				"text": "So instead of bullying someone\nbecause they're different"
+			},
+			{
+				"id": "5",
+				"startTime": 10877,
+				"endTime": 12545,
+				"text": "ask questions instead."
+			},
+			{
+				"id": "6",
+				"startTime": 12545,
+				"endTime": 16583,
+				"text": "If someone in your class celebrates\na different holiday than you ask them"
+			},
+			{
+				"id": "7",
+				"startTime": 16583,
+				"endTime": 21755,
+				"text": "about it. Or if the new girl at school\nhas an accent, ask where she's from."
+			},
+			{
+				"id": "8",
+				"startTime": 22022,
+				"endTime": 25992,
+				"text": "Sometimes kids are bullied\nbecause they are seen as not being “normal”."
+			},
+			{
+				"id": "9",
+				"startTime": 25992,
+				"endTime": 28461,
+				"text": "But really, what is normal?"
+			},
+			{
+				"id": "10",
+				"startTime": 28461,
+				"endTime": 30397,
+				"text": "Just because your family acts"
+			},
+			{
+				"id": "11",
+				"startTime": 30397,
+				"endTime": 33833,
+				"text": "or looks a certain way doesn't mean that's\nwhat's normal."
+			},
+			{
+				"id": "12",
+				"startTime": 34134,
+				"endTime": 36703,
+				"text": "We should always be kind to one another."
+			},
+			{
+				"id": "13",
+				"startTime": 36703,
+				"endTime": 39873,
+				"text": "Being kind to\nsomeone can be as simple as saying hi,"
+			},
+			{
+				"id": "14",
+				"startTime": 40073,
+				"endTime": 42976,
+				"text": "smiling at them\nor giving them a compliment."
+			},
+			{
+				"id": "15",
+				"startTime": 42976,
+				"endTime": 45979,
+				"text": "It could also be including\nsomeone new to your group"
+			},
+			{
+				"id": "16",
+				"startTime": 45979,
+				"endTime": 49249,
+				"text": "at school,\nor standing up for someone being bullied."
+			}
+		],
+		"Outro 2": [
+			{
+				"id": "1",
+				"startTime": 100,
+				"endTime": 4571,
+				"text": "So the next time you see someone who looks\ndifferent than you, remember to be kind."
+			},
+			{
+				"id": "2",
+				"startTime": 4637,
+				"endTime": 9175,
+				"text": "A little kindness goes a long way and\ncan help make your school a better place."
+			}
+		]
+	}
+}
