7a7a5fff374dd56e6c45a7c45ed7948ffeb5fc2c
Jeff Blume <jblume@childrenswi.org>
2026-08-05 15:01:39 -0500

    Add whatisaconcussion-et/1

---
+++ activities/whatisaconcussion-et/1/audio/vo-captions.js
@@ -0,0 +1,244 @@
+export const Captions = {
+	"eng": {
+		"silence": [
+			{
+				"id": "1",
+				"startTime": 66,
+				"endTime": 65,
+				"text": ""
+			}
+		],
+		"slide 1": [
+			{
+				"id": "1",
+				"startTime": 66,
+				"endTime": 2635,
+				"text": "By the end of this “What is a \nConcussion?” module?"
+			},
+			{
+				"id": "2",
+				"startTime": 2635,
+				"endTime": 6339,
+				"text": "You will understand the definition\nof concussion and how a concussion occurs."
+			}
+		],
+		"slide 3": [
+			{
+				"id": "1",
+				"startTime": 66,
+				"endTime": 4437,
+				"text": "Medical providers describe a concussion\nas a mild traumatic brain injury"
+			},
+			{
+				"id": "2",
+				"startTime": 4437,
+				"endTime": 7540,
+				"text": "because concussions are usually not life\nthreatening."
+			},
+			{
+				"id": "3",
+				"startTime": 8074,
+				"endTime": 13313,
+				"text": "A concussion may result in confusion,\namnesia or even a loss of consciousness."
+			},
+			{
+				"id": "4",
+				"startTime": 13780,
+				"endTime": 16950,
+				"text": "Most concussions\ndo not involve a loss of consciousness,"
+			},
+			{
+				"id": "5",
+				"startTime": 17050,
+				"endTime": 20653,
+				"text": "and it's important to know that\na loss of consciousness does not indicate"
+			},
+			{
+				"id": "6",
+				"startTime": 20653,
+				"endTime": 24557,
+				"text": "a more severe injury or increased\nrisk for worse outcomes."
+			},
+			{
+				"id": "7",
+				"startTime": 24991,
+				"endTime": 29396,
+				"text": "However, the effects of a concussion\nneed to be taken seriously as improper"
+			},
+			{
+				"id": "8",
+				"startTime": 29396,
+				"endTime": 31031,
+				"text": "management can lead to prolonged"
+			},
+			{
+				"id": "9",
+				"startTime": 31031,
+				"endTime": 35402,
+				"text": "symptoms, permanent functional\ndeficits and in rare cases, death."
+			}
+		],
+		"slide 4": [
+			{
+				"id": "1",
+				"startTime": 66,
+				"endTime": 4270,
+				"text": "A concussion occurs at the cellular level\nand is not a structural injury."
+			},
+			{
+				"id": "2",
+				"startTime": 4571,
+				"endTime": 8475,
+				"text": "Typically, a concussion is associated\nwith normal structural neuroimaging"
+			},
+			{
+				"id": "3",
+				"startTime": 8508,
+				"endTime": 13947,
+				"text": "findings, which means that a concussion\ndoes not show up on a CT scan or MRI."
+			},
+			{
+				"id": "4",
+				"startTime": 14514,
+				"endTime": 17984,
+				"text": "As the brain attempts to heal itself\nafter a concussion occurs,"
+			},
+			{
+				"id": "5",
+				"startTime": 17984,
+				"endTime": 21788,
+				"text": "it needs more resources and energy\nthan the body can provide,"
+			},
+			{
+				"id": "6",
+				"startTime": 21821,
+				"endTime": 24758,
+				"text": "which leads to an energy crisis."
+			},
+			{
+				"id": "7",
+				"startTime": 24758,
+				"endTime": 28228,
+				"text": "This energy crisis leads to\nmultiple signs and symptoms."
+			}
+		],
+		"slide 5 correct": [
+			{
+				"id": "1",
+				"startTime": 100,
+				"endTime": 1101,
+				"text": "That's correct."
+			}
+		],
+		"slide 5 incorrect": [
+			{
+				"id": "1",
+				"startTime": 100,
+				"endTime": 1668,
+				"text": "That's not correct."
+			},
+			{
+				"id": "2",
+				"startTime": 1668,
+				"endTime": 4504,
+				"text": "A concussion is a type\nof mild traumatic brain injury."
+			}
+		],
+		"slide 5 question": [
+			{
+				"id": "1",
+				"startTime": 66,
+				"endTime": 3470,
+				"text": "A concussion is a type of mild\ntraumatic brain injury."
+			},
+			{
+				"id": "2",
+				"startTime": 3770,
+				"endTime": 5105,
+				"text": "True"
+			},
+			{
+				"id": "3",
+				"startTime": 5105,
+				"endTime": 6039,
+				"text": "False"
+			}
+		],
+		"slide 6 incorrect": [
+			{
+				"id": "1",
+				"startTime": 300,
+				"endTime": 1735,
+				"text": "That's incorrect."
+			},
+			{
+				"id": "2",
+				"startTime": 1735,
+				"endTime": 4170,
+				"text": "The brain bouncing around\nand twisting in the skull"
+			},
+			{
+				"id": "3",
+				"startTime": 4170,
+				"endTime": 8241,
+				"text": "and a bump, blow\nor jolt to the head or hit to the body"
+			},
+			{
+				"id": "4",
+				"startTime": 8274,
+				"endTime": 10143,
+				"text": "lead to chemical changes in the brain."
+			}
+		],
+		"slide 6 question": [
+			{
+				"id": "1",
+				"startTime": 66,
+				"endTime": 3069,
+				"text": "Which of these leads to chemical changes\nin the brain?"
+			},
+			{
+				"id": "2",
+				"startTime": 3103,
+				"endTime": 6106,
+				"text": "The brain\nbouncing around and twisting in the skull"
+			},
+			{
+				"id": "3",
+				"startTime": 6373,
+				"endTime": 9709,
+				"text": "A bump, blow or jolt to the head\nor a hit to the body"
+			},
+			{
+				"id": "4",
+				"startTime": 10510,
+				"endTime": 13513,
+				"text": "Being pulled from sports and activities"
+			},
+			{
+				"id": "5",
+				"startTime": 13880,
+				"endTime": 16282,
+				"text": "The brain\nbouncing around and twisting in the skull"
+			},
+			{
+				"id": "6",
+				"startTime": 16282,
+				"endTime": 20253,
+				"text": "and a bump, blow or jolt to the head\nor a hit to the body"
+			},
+			{
+				"id": "7",
+				"startTime": 21021,
+				"endTime": 24190,
+				"text": "Being pulled from sports and activities\nand a bump, blow"
+			},
+			{
+				"id": "8",
+				"startTime": 24190,
+				"endTime": 26426,
+				"text": "or jolt to the head or a hit to the body"
+			}
+		]
+	}
+}
